b416ae0ac6d7c16f43a6e3e6c3f42f7308363234
Takuya Aramaki <tar.yaki71@gmail.com>
2019-05-24 16:56:29 +0900

    improve init-windows.el (add comments, IME settings and font existance check)

--- init-windows.el
+++ init-windows.el
@@ -1,16 +1,19 @@
+;;; init-windows.el --- supplemental init.el for Windows
+;;; Commentary:
+;;; Code:
 ;; setting for tramp
 (eval-when-compile (require 'tramp))
+(defun get-ip-address ()
+  "Win32: get the IP-address of the first network interface."
+  (let ((cmd (if (string-match "bash" shell-file-name)
+                 "ifconfig | grep Bcast" "ipconfig | findstr IPv4")))
+    (let ((ipconfig (shell-command-to-string cmd)))
+      (string-match "\\(\\([0-9]+.\\)+[0-9]+\\)" ipconfig)
+      (match-string 0 ipconfig))))
 (with-eval-after-load 'tramp
   (setenv "PATH" (concat "C:\\Program Files (x86)\\PuTTY" ";" (getenv "PATH")))
   (setq tramp-default-method "plink")
   ;; tunneling
-  (defun get-ip-address ()
-    "Win32: get the IP-address of the first network interface"
-    (let ((cmd (if (string-match "bash" shell-file-name)
-                   "ifconfig | grep Bcast" "ipconfig | findstr IPv4")))
-      (let ((ipconfig (shell-command-to-string cmd)))
-        (string-match "\\(\\([0-9]+.\\)+[0-9]+\\)" ipconfig)
-        (match-string 0 ipconfig))))
   (when (not (string-match "^133\\.103\\.101\\." (get-ip-address)))
     (add-to-list 'tramp-default-proxies-alist
                  '("uv20" nil "/plink:fe1:"))
@@ -19,8 +22,8 @@
     (add-to-list 'tramp-default-method-alist
                  '("uv20\\|ubuntu" nil "ssh"))))
 
-(set-frame-font "Ricty Diminished-12")
-(set-default-font "Ricty Diminished-12")
+(when (member "Ricty Diminished" (font-family-list))
+  (set-frame-font "Ricty Diminished-11"))
 
 (setq initial-frame-alist
       (append (list '(width . 82))
@@ -28,3 +31,12 @@
 
 (setq shell-file-name "C:/Windows/System32/bash.exe"
       multi-term-program shell-file-name)
+
+;; IME settings
+(w32-ime-initialize)
+(setq default-input-method "W32-IME")
+(setq-default w32-ime-mode-line-format-original "[--]")
+(setq w32-ime-mode-line-state-indicator-list '("[--]" "[あ]" "[--]"))
+
+(provide 'init-windows)
+;;; init-windows.el ends here
